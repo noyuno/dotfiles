cb5d7c744d3a3174e2909b8f397e4600c4e1ca4b
noyuno <noyuno@users.noreply.github.com>
2018-02-18 20:50:36 +0900

    Update

--- raspberry-pi/python.sh
+++ raspberry-pi/python.sh
@@ -2,17 +2,18 @@
 
 python()
 {
-    v=3.6.2
-    dfx wget -qO $HOME/Python-$v.tgz https://www.python.org/ftp/python/$v/Python-$v.tgz
-    pushd $HOME
-        dfx tar xzf $HOME/Python-$v.tgz
-        pushd $HOME/Python-$v/
-            dfx ./configure
-            dfx make -j4
-            dfx sudo make install
-        popd
-    popd
+    #v=3.6.2
+    #dfx wget -qO $HOME/Python-$v.tgz https://www.python.org/ftp/python/$v/Python-$v.tgz
+    #pushd $HOME
+    #    dfx tar xzf $HOME/Python-$v.tgz
+    #    pushd $HOME/Python-$v/
+    #        dfx ./configure
+    #        dfx make -j4
+    #        dfx sudo make install
+    #    popd
+    #popd
 
-    dfx sudo -H pip3 install watchdog tornado icalendar
+    dfx sudo apt install python3 python3-dev
+    dfx sudo -H pip3 install watchdog tornado icalendar rpi.gpio pychromecast
 }
 
