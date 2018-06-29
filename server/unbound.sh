dd7eec3c9933f945bb81225fddcc929a495fcf3c
noyuno <noyuno@users.noreply.github.com>
2018-06-30 00:58:16 +0900

    Update

---
+++ server/unbound.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+. ~/dotfiles/bin/dffunc
+
+unbound () {
+    aptinstall unbound
+    dfx sudo systemctl stop systemd-resolved
+    dfx sudo systemctl disable systemd-resolved
+    dfx sudo cp ~/dotfiles/server/unbound.conf /etc/unbound/unbound.conf.d/noyuno.space.conf
+    cat << EOF | sudo tee /etc/resolv.conf
+nameserver 127.0.0.1
+EOF
+    dfx sudo systemctl enable unbound
+    dfx sudo systemctl start unbound
+}
+
